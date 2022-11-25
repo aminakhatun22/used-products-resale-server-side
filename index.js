42cd653c088ad19b3a2bd67d537100274ed56988
aminakhatun22 <aminak26802@gmail.com>
2022-11-25 17:10:13 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const cors = require('cors');
+const port = process.env.PORT || 5000;
+
+
+
+const app = express();
+
+
+//  middleware 
+app.use(cors()); 0
+app.use(express.json());
+
+
+app.get('/', async (req, res) => {
+    res.send('furnob server is running');
+})
+
+app.listen(port, () => console.log(`Furnob server is running on ${port}`));
