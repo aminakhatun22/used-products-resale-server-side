f3608a40d2563bfc5de36d8e643b74dae6e108e7
aminakhatun22 <aminak26802@gmail.com>
2022-11-25 19:52:05 +0600

    categories-collection done

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
 const port = process.env.PORT || 5000;
 
 
@@ -10,6 +12,30 @@ const app = express();
 //  middleware 
 app.use(cors()); 0
 app.use(express.json());
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.a3goieg.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+async function run() {
+    try {
+        const categoriesCollection = client.db('furnob').collection('Categories')
+
+        app.get('/Categories', async (req, res) => {
+            const query = {};
+            const options = await categoriesCollection.find(query).toArray();
+            res.send(options);
+        })
+    }
+    finally {
+
+    }
+}
+run().catch(console.log);
+
+
+
 
 
 app.get('/', async (req, res) => {
